6504ee5c89fa2588a615e0825473e2cfa771d5fd
bagar0x60 <leha969696@yandex.ru>
2018-10-08 18:29:33 +0300

    add strategy change (maybe i should not)

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,11 @@ impl Field {
     }
 }
 
-
+#[derive(Clone)]
+enum RelaxationStrategy {
+    Stack,
+    ArrayTraversing,
+}
 
 
 
@@ -75,6 +79,8 @@ pub struct SandPile {
     field: Field,
     sand: Vec<Vec<u32>>,
     stack: Vec<(usize, usize)>,
+    relaxation_strategy: RelaxationStrategy,
+    is_relaxed: bool,
 }
 
 impl SandPile {
@@ -82,51 +88,99 @@ impl SandPile {
         let (x_max, y_max) = field.size;
         let sand = vec![vec![sand_level; x_max]; y_max];
         let mut stack: Vec<(usize, usize)> = Vec::new();
-        if sand_level > 3 {
-            for x in 0..x_max {
-                for y in 0..y_max {
-                    if field.is_inside((x, y)) {
-                        stack.push((x, y));
-                    }
+        let relaxation_strategy = RelaxationStrategy::ArrayTraversing;
+        let is_relaxed = sand_level <= 3;
+
+        SandPile {field, sand, stack, relaxation_strategy, is_relaxed}
+    }
+
+    fn build_stack(&mut self) {
+        self.stack = Vec::new();
+        for x in 0..self.field.size.0 {
+            for y in 0..self.field.size.1 {
+                if self.field.is_inside((x, y)) && self.sand[y][x] > 3 {
+                    self.stack.push((x, y))
                 }
             }
         }
-
-        return SandPile {field, sand, stack}
     }
 
-    fn topple(&mut self, rounds: u32) {
-        for _ in 0..rounds {
-            if self.stack.is_empty() {
-                return;
-            }
+    fn topple(&mut self) {
+        if self.is_relaxed() {
+            return;
+        }
 
-            let mut new_stack: Vec<(usize, usize)> = Vec::new();
-            while let Some((x, y)) = self.stack.pop() {
-                if self.sand[y][x] > 3 {
-                    self.sand[y][x] -= 4;
+        let FIELD_SIZE =  self.field.size.0 * self.field.size.1;
+        let relaxation_strategy = self.relaxation_strategy.clone();
+        /*
+        match self.relaxation_strategy {
+            ref ArrayTraversing => println!("1"),
+            ref Stack => println!("2")
+        };
+        */
 
-                    for (x_neighbor, y_neighbor) in self.field.neighbours[y][x].iter() {
-                        self.sand[*y_neighbor][*x_neighbor] += 1;
-                        if self.sand[*y_neighbor][*x_neighbor] > 3 {
-                            new_stack.push((*x_neighbor, *y_neighbor))
+        match relaxation_strategy {
+            RelaxationStrategy::ArrayTraversing => {
+                let mut topplings_count = 0;
+                for x in 0..self.field.size.0 {
+                    for y in 0..self.field.size.1 {
+                        if self.field.is_inside((x, y)) && self.sand[y][x] > 3 {
+                            topplings_count += 1;
+                            self.sand[y][x] -= 4;
+
+                            for (x_neighbor, y_neighbor) in self.field.neighbours[y][x].iter() {
+                                self.sand[*y_neighbor][*x_neighbor] += 1;
+                            }
                         }
                     }
                 }
-            }
 
-            self.stack = new_stack;
+                // println!("ArrayTraversing {} {}", topplings_count, FIELD_SIZE);
+                self.is_relaxed = topplings_count == 0;
+
+                if topplings_count < FIELD_SIZE / 8 {
+                    println!("Change strategy to Stack {} {}", topplings_count, FIELD_SIZE);
+                    self.build_stack();
+                    self.relaxation_strategy = RelaxationStrategy::Stack;
+                }
+            },
+            RelaxationStrategy::Stack => {
+
+                // println!("Stack");
+                let mut new_stack: Vec<(usize, usize)> = Vec::new();
+
+                while let Some((x, y)) = self.stack.pop() {
+                    if self.sand[y][x] > 3 {
+                        self.sand[y][x] -= 4;
+
+                        for (x_neighbor, y_neighbor) in self.field.neighbours[y][x].iter() {
+                            self.sand[*y_neighbor][*x_neighbor] += 1;
+                            if self.sand[*y_neighbor][*x_neighbor] > 3 {
+                                new_stack.push((*x_neighbor, *y_neighbor))
+                            }
+                        }
+                    }
+                }
+
+                self.stack = new_stack;
+                self.is_relaxed = self.stack.is_empty();
+
+                if self.stack.len() > FIELD_SIZE {
+                    self.relaxation_strategy = RelaxationStrategy::ArrayTraversing;
+                }
+            }
         }
     }
 
+
     fn relax(&mut self) {
-        while ! self.stack.is_empty() {
-            self.topple(1);
+        while ! self.is_relaxed() {
+            self.topple();
         }
     }
 
     fn is_relaxed(&self) -> bool {
-        self.stack.is_empty()
+        self.is_relaxed
     }
 
     fn add_grains(&mut self, (x, y): (usize, usize), count: u32) {
@@ -134,6 +188,7 @@ impl SandPile {
             self.sand[y][x] += count;
             if self.sand[y][x] > 3 {
                 self.stack.push((x, y));
+                self.is_relaxed = false;
             }
         }
     }
@@ -142,10 +197,10 @@ impl SandPile {
 
 
 use std::ops;
-impl ops::Add<SandPile> for SandPile {
+impl<'a> ops::Add for &'a SandPile {
     type Output = SandPile;
 
-    fn add(self, other: SandPile) -> SandPile {
+    fn add(self, other: &'a SandPile) -> SandPile {
         if self.field.size != other.field.size {
             panic!("Sum of SandPiles of different field sizes isn't allowed");
         }
@@ -159,10 +214,10 @@ impl ops::Add<SandPile> for SandPile {
     }
 }
 
-impl ops::Sub for SandPile {
+impl<'a> ops::Sub for &'a SandPile {
     type Output = SandPile;
 
-    fn sub(self, other: SandPile) -> SandPile {
+    fn sub(self, other: &'a SandPile) -> SandPile {
         if self.field.size != other.field.size {
             panic!("Sum of SandPiles of different field sizes isn't allowed");
         }
@@ -197,9 +252,14 @@ impl App {
         const YELLOW:[f32; 4] = [1.0, 1.0, 0.0, 1.0];
 
 
+        const BLUE3: [f32; 4] = [0.0, 0.0, 0.8, 1.0];
+        const BLUE2: [f32; 4] = [0.2, 0.2, 1.0, 1.0];
+        const BLUE1: [f32; 4] = [0.6, 0.6, 1.0, 1.0];
+
+
         let square_size: f64 = ((args.height as f64) / (sand.len() as f64))
-                                    .min((args.width as f64) / (sand[0].len() as f64))
-                                    .floor();
+                                    .min((args.width as f64) / (sand[0].len() as f64));
+
         let square = rectangle::square(0.0, 0.0, square_size);
 
 
@@ -209,10 +269,10 @@ impl App {
             for (y, line) in sand.iter().enumerate() {
                 for (x, cell) in line.iter().enumerate() {
                     let colour = match *cell  {
-                        0 => GREEN,
-                        1 => YELLOW,
-                        2 => BLUE,
-                        3 => RED,
+                        0 => WHITE,
+                        1 => BLUE1,
+                        2 => BLUE2,
+                        3 => BLUE3,
                         _ => BLACK
                     };
                     let transform = c.transform.trans(square_size*(x as f64), square_size*(y as f64));
@@ -223,6 +283,46 @@ impl App {
         });
     }
 }
+
+
+pub struct Visualizer {
+    app: App,
+    window: Window,
+}
+
+impl Visualizer {
+    fn visualize_relaxation(&mut self, sand_pile: &mut SandPile) {
+        let mut events = Events::new(EventSettings::new());
+
+        while ! sand_pile.is_relaxed() {
+            if let Some(e) = events.next(&mut self.window) {
+                if let Some(r) = e.render_args() {
+                    self.app.render(&r, &sand_pile.sand);
+                }
+            }
+            sand_pile.topple();
+        }
+    }
+
+    fn loop_draw(&mut self, sand_pile: &SandPile) {
+        let mut events = Events::new(EventSettings::new());
+        while let Some(e) = events.next(&mut self.window) {
+            if let Some(r) = e.render_args() {
+                self.app.render(&r, &sand_pile.sand);
+            }
+        }
+    }
+}
+
+fn compute_identity(field: &Field, visualizer: &mut Visualizer) {
+    let mut sand_pile1 = SandPile::new(field.clone(), 6);
+    let mut sand_pile2 = SandPile::new(field.clone(), 6);
+
+    visualizer.visualize_relaxation(&mut sand_pile1);
+    sand_pile2 = &sand_pile2 - &sand_pile1;
+    visualizer.visualize_relaxation(&mut sand_pile2);
+}
+
 
 fn main() {
     // Change this to OpenGL::V2_1 if not working.
@@ -238,35 +338,33 @@ fn main() {
         .build()
         .unwrap();
 
-    // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
     };
 
-    let mut sand_pile1 = SandPile::new(Field::square(200), 6);
-    let mut sand_pile = SandPile::new(Field::square(200), 6);
+    let mut visualizer = Visualizer {app, window};
 
-    sand_pile1.relax();
-    sand_pile = sand_pile - sand_pile1;
+    let field = Field::square(400);
 
-    // sand_pile.add_grains((50, 50), 1_000_000);
-    // sand_pile.add_grains((75, 75), 2);
+    compute_identity(&field, &mut visualizer);
 
 
+    /*
+    let mut sand_pile = SandPile::new(field.clone(), 0);
+    sand_pile.add_grains((100, 100), 2);
 
-    let mut events = Events::new(EventSettings::new());
-    loop {
-        if
-
-
-
-        if let Some(e) = events.next(&mut window){
-            if let Some(r) = e.render_args() {
-                app.render(&r, &sand_pile.sand);
-            }
-        }
-
-        sand_pile.topple(1);
-
+    // 2^32 grains in center
+    for i in 0..32 {
+        println!("{}", i);
+        sand_pile = &sand_pile + &sand_pile;
+        visualizer.visualize_relaxation(&mut sand_pile);
     }
+    */
+
+
+    //sand_pile = sand_pile - sand_pile1;
+
+    // visualizer.visualize_relaxation(&mut sand_pile);
+
+    // visualizer.loop_draw(&sand_pile);
 }
